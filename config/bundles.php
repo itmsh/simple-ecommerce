9f81888efbc20f509124c49e47ce5fefad0c889a
masoud sharifi <masoudsharifi@MacBook-Air.local>
2018-08-31 10:15:28 +0430

    doctrine and elasticsearch is added

--- config/bundles.php
+++ config/bundles.php
@@ -6,4 +6,8 @@ return [
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    FOS\ElasticaBundle\FOSElasticaBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
 ];
--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -5,6 +5,8 @@ namespace App\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Cache\Adapter\AdapterInterface;
+
 
 class ProductController extends AbstractController
 {
@@ -19,8 +21,16 @@ class ProductController extends AbstractController
     /**
      * @Route("/تست/{productName}", options={"utf8": true})
      */
-    public function show($productName)
+    public function show($productName, AdapterInterface $cache)
     {
+
+        $item = $cache->getItem('markdown_'.md5($productName));
+        if (!$item->isHit()) {
+            $item->set($productName);
+            $cache->save($item);
+        }
+        $productName = $item->get();
+
         return $this->render('product/show.html.twig', [
             'title' => ucwords(str_replace('-', ' ', $productName)),
         ]);
